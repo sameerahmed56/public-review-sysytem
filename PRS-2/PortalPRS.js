fbeda1c2d609d9ec3dd5f24064dea5b792600a5b
Sameer Ahmed <54800755+sameerahmed56@users.noreply.github.com>
2020-06-08 12:54:21 +0530

    first-branch-addition

---
+++ PRS-2/PortalPRS.js
@@ -0,0 +1,135 @@
+var myApp = angular.module("myApp", ['ui.router']);
+
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('dashboardmessage', {
+            // url: '/dashboard/:e/:f',
+            url:'/',
+            templateUrl: "dashboard.html",
+            controller: 'thirdController'
+        })
+        .state('profilemessage', {
+            url: '/profile/:a/:b',
+            templateUrl: 'profile.html',
+            controller: 'firstController'
+
+        })
+        .state('reviewmessage', {
+            url: '/review/:c/:d',
+            templateUrl: 'review.html',
+            controller: 'secondController'
+
+        })
+        .state('topprojectsmessage', {
+            url: '/topprojects/:g/:h',
+            templateUrl: 'topprojects.html',
+            controller: 'fourthController'
+
+        })
+        // .state('root', {
+        //     url: '/',
+        //     template: "YOU ARE AT ROOT"
+        // })
+
+    $urlRouterProvider.otherwise('/');
+
+});
+
+myApp.controller('firstController', function ($scope, $stateParams) {
+    $scope.a = $stateParams.a,
+        $scope.b = $stateParams.b
+})
+
+myApp.controller('secondController', function ($scope, $stateParams) {
+    $scope.c = $stateParams.c,
+    $scope.d = $stateParams.d,
+    $scope.rate ='';
+
+})
+myApp.controller('thirdController', function ($scope, $stateParams) {
+    $scope.e = $stateParams.e,
+        $scope.f = $stateParams.f
+})
+myApp.controller('fourthController', function ($scope, $stateParams) {
+    $scope.g = $stateParams.g,
+        $scope.h = $stateParams.h
+})
+function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    // document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    // document.getElementById("main").style.marginLeft= "0";
+  }
+//   ----
+$(document).ready(function () {
+    $(".gear-btn").click(function () {
+        $(".logout-panel").fadeToggle("slow");
+    });
+});
+// ------
+function logOut(){
+    window.location = "LoginPRS.html";
+}
+// -------
+function displayProfile() {
+    fetch("http://jsonplaceholder.typicode.com/users/2")
+        .then(function (response) {
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                throw new Error('Invalid user ID');
+            }
+        })
+        .then((data) => {
+            document.getElementById('nameOut').innerHTML = data.name;
+            document.getElementById('emailOut').innerHTML = data.email;
+            document.getElementById('phoneOut').innerHTML = data.phone;
+            var addressData = data.address.suite + ", " + data.address.street + ", " + data.address.city;
+            document.getElementById('addressOut').innerHTML = addressData;
+            document.getElementById('zipcodeOut').innerHTML = data.address.zipcode;
+            document.getElementById('websiteOut').innerHTML = data.website;
+        })
+        .catch((err) => {
+            console.log('ERROR: ', err.message);
+        });
+
+}  
+
+function displayDashboard(){
+    fetch("http://jsonplaceholder.typicode.com/users/2")
+        .then(function (response) {
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                throw new Error('Invalid user ID');
+            }
+        })
+        .then((data) => {
+            document.getElementById('nameOut').innerHTML = data.name;
+        })
+        .catch((err) => {
+            console.log('ERROR: ', err.message);
+        });
+}
+function displayReview() {
+    fetch("http://jsonplaceholder.typicode.com/users/2")
+        .then(function (response) {
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                throw new Error('Invalid user ID');
+            }
+        })
+        .then((data) => {
+            var addressData = data.address.suite + ", " + data.address.street + ", " + data.address.city;
+            console.log(addressData);
+            document.getElementById('addressOut').innerHTML = addressData; 
+            document.getElementById('cityOut').innerHTML = data.address.city;   
+            })
+        .catch((err) => {
+            console.log('ERROR: ', err.message);
+        });
+}
---
+++ PRS-2/RegisterPRS.js
@@ -0,0 +1,171 @@
+function validateNumber(a, b) {
+    var mobileNumber = document.getElementById(a).value;
+    const mobileNumberValue = mobileNumber.trim();
+    if (mobileNumberValue == "") {
+        document.getElementById(b).innerHTML = "Enter Your Mobile Number";
+    }
+    else if (mobileNumberValue.length != 10) {
+        document.getElementById(b).innerHTML = "Must be of 10 digits";
+    }
+    else if ((mobileNumberValue.charAt(0) != 9) && (mobileNumberValue.charAt(0) != 8) && (mobileNumberValue.charAt(0) != 7) && (mobileNumberValue.charAt(0) != 6)) {
+        document.getElementById(b).innerHTML = "Mobile No.start with 6,7,8,9";
+
+    }
+    else {
+        document.getElementById(b).innerHTML = "";
+        return true;
+    }
+}
+function validateName(a, b) {
+    var name = document.getElementById(a).value;
+    // const namValue = name.trim();
+    var alphabet = /^[A-Za-z]+$/;
+    if (name.match(alphabet)) {
+        document.getElementById(b).innerHTML = "";
+        return true;
+    }
+    else{
+        document.getElementById(b).innerHTML = "Enter Valid Name";
+    }
+}
+function validateAadharNumber(a, b) {
+    var aadharNumber = document.getElementById(a).value;
+    const aadharNumberValue = aadharNumber.trim();
+    if (aadharNumberValue == "") {
+        document.getElementById(b).innerHTML = "Enter Your Aadhar Number";
+    }
+    else if ((aadharNumberValue.charAt(0) != 9) && (aadharNumberValue.charAt(0) != 8) && (aadharNumberValue.charAt(0) != 7) && (aadharNumberValue.charAt(0) != 6) && (aadharNumberValue.charAt(0) != 5) && (aadharNumberValue.charAt(0) != 4) && (aadharNumberValue.charAt(0) != 3) && (aadharNumberValue.charAt(0) != 2) && (aadharNumberValue.charAt(0) != 1)) {
+        document.getElementById(b).innerHTML = "Adhar Number Can't Start with 0";
+    }
+    else if (aadharNumberValue.length != 12){
+        document.getElementById(b).innerHTML = "Must Be of 12 Digit";
+    }
+    else {
+        document.getElementById(b).innerHTML = "";
+        return true;
+    }
+}
+function validateEmail(a, b) {
+    var email = document.getElementById(a).value;
+    var emailValue = email.trim();
+    var arr = emailValue.split("@");
+    var alphabet = /^[A-Za-z0-9@|.]+$/;
+    
+    if (emailValue.match(alphabet)){
+        if (emailValue == "") {
+            document.getElementById(b).innerHTML = "Enter Valid Email";
+        }
+        else if ((emailValue.indexOf('@') == 0) || (emailValue.indexOf('.') == 0)) {
+            document.getElementById(b).innerHTML = "Enter Valid Email";
+        }
+        else if ((emailValue.charAt(0) == 9) || (emailValue.charAt(0) == 8) || (emailValue.charAt(0) == 7) || (emailValue.charAt(0) == 6) || (emailValue.charAt(0) == 5) || (emailValue.charAt(0) == 4) || (emailValue.charAt(0) == 3) || (emailValue.charAt(0) == 2) || (emailValue.charAt(0) == 1) || (emailValue.charAt(0) == 0)) {
+            document.getElementById(b).innerHTML = "Enter Valid Email";
+        }
+        else if (arr.length > 2) {
+            document.getElementById(b).innerHTML = "Enter Valid Email";
+        }
+        else if (emailValue.lastIndexOf('.') - emailValue.indexOf('@') < 5) {
+            document.getElementById(b).innerHTML = "Enter Valid Email";
+        }
+        else if (emailValue.lastIndexOf('.') >= (emailValue.length - 2)) {
+            document.getElementById(b).innerHTML = "Enter Valid Email";
+        }
+        else if ((emailValue.indexOf('@') == -1) || (emailValue.indexOf('.') == -1)) {
+            document.getElementById(b).innerHTML = "Enter Valid Email";
+        }
+        
+        
+        else {
+            document.getElementById(b).innerHTML = "";
+            return true;
+        }
+    }
+    else {
+        document.getElementById(b).innerHTML = "Enter Valid Email";
+    }
+}
+
+function validateUsername(a,b){
+    var username =  document.getElementById(a).value;
+    var usernameValue = username.trim();
+    
+    if (usernameValue == "") {
+        document.getElementById(b).innerHTML = "Enter your Username";
+    }
+    else if ((usernameValue.charAt(0) == 9) || (usernameValue.charAt(0) == 8) || (usernameValue.charAt(0) == 7) || (usernameValue.charAt(0) == 6) || (usernameValue.charAt(0) == 5) || (usernameValue.charAt(0) == 4) || (usernameValue.charAt(0) == 3) || (usernameValue.charAt(0) == 2) || (usernameValue.charAt(0) == 1) || (usernameValue.charAt(0) == 0)) {
+        document.getElementById(b).innerHTML = "Username Can't Start With A Number";
+
+    }
+    else if (usernameValue.length < 5) {
+        document.getElementById(b).innerHTML = "Username Must be of 4 Digits";
+    }
+    else {
+        document.getElementById(b).innerHTML = "";
+        return true;
+    }
+   
+
+} 
+function validatePassword(a,b){
+    var password = document.getElementById(a).value;
+    
+    if (password == ""){
+        document.getElementById(b).innerHTML = "Enter Your Password";
+    }
+    else if(password.length < 6 ){
+        document.getElementById(b).innerHTML = "Password must be of 6 digits";
+    }
+    else{
+        document.getElementById(b).innerHTML = "";
+        return true;
+    }
+
+}
+
+function validateCnfPassword(a,b){
+    var password = document.getElementById('password').value;
+    var cnfPassword = document.getElementById('cnfpassword').value;
+    if (cnfPassword == "") {
+        document.getElementById(b).innerHTML = "Re-Enter Your Password";
+    }
+    else if (password != cnfPassword){
+        document.getElementById('cnfpassworderror').innerHTML = "Passwords do not match";
+    }
+    else {
+        document.getElementById(b).innerHTML = "";
+        return true;
+    }
+}
+//     else if ((Value.charAt(0) == 9) || (Value.charAt(0) == 8) || (Value.charAt(0) == 7) || (Value.charAt(0) == 6) || (Value.charAt(0) == 5) || (Value.charAt(0) == 4) || (Value.charAt(0) == 3) || (Value.charAt(0) == 2) || (Value.charAt(0) == 1) || (Value.charAt(0) == 0)) {
+//     document.getElementById(b).innerHTML = "Email Can't Start With A Number";
+
+// }
+
+function onClick() {
+    var q = validateDob('dob','doberror');
+    var r = validateUsername('username','usernameerror');
+    var s = validateName('firstname', 'firstnameerror');
+    var t = validateName('lastname', 'lastnameerror');
+    var u = validateCnfPassword('cnfpassword','cnfpassworderror')
+    var v = validateName('fname', 'fnameerror');
+    var w = validateNumber('mobilenumber', 'mobilenumbererror');
+    var x = validatePassword('password', 'passworderror')
+    var y = validateEmail('email','emailerror');
+    var z = validateAadharNumber('aadharnumber','aadharnumbererror');
+    if ((q == true) && (r == true) && (s == true) && (t == true) && (u == true) && (v == true) && (w == true) && (x == true) && (y == true) && (z == true)) {
+        window.location="LogInImg.html";    }
+    else {
+        alert("Fill All Fields Correctly");
+    }
+
+}
+function validateDob(a,b){
+    var dob = document.getElementById(a).value;
+    if (dob = "") {
+        document.getElementById(b).innerHTML = "Enter your DOB";
+    }
+    else{
+        document.getElementById(b).innerHTML = "";
+        return true;
+    }
+}
